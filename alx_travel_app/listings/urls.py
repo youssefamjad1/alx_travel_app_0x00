efd12f61344dc746b4b287be7138ef10e99db53d
Youssef HIMMI <yhimmi1@gmail.com>
2025-05-18 22:48:35 +0200

    Initial project setup with MySQL, Swagger, CORS, and DRF

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,22 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from alx_travel_app.listings import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Travel App API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('api/', include('alx_travel_app.listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
